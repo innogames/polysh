65441751a045abfad97e0e445636b624a0bd7824
gchazara@trinidad.inria.fr <>
2006-10-15 19:31:37 +0200

    Small optimization

--- gsh/stdin_reader.py
+++ gsh/stdin_reader.py
@@ -32,7 +32,11 @@ class stdin_reader(buffered_dispatcher):
             console_output('stdin read buffer too large\n', sys.stderr)
             raise asyncore.ExitNow
 
-        lf_pos = self.read_buffer.find('\n')
+        lf_pos = new_data.find('\n')
+        if lf_pos >= 0:
+            # Optimization: there were no '\n' in the previous buffer, so
+            # we search only on the new data, and offset the position
+            lf_pos += len(self.read_buffer) - len(new_data)
         while lf_pos >= 0:
             line = self.read_buffer[:lf_pos + 1]
             for r in remote_dispatcher.all_instances():
