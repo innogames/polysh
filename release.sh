3149dfd16ce001a338a3e9bc4f336747d77c3c76
g@localhost.localdomain <>
2006-10-20 23:52:17 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+set -e # Exit on error
+
+PACKAGE=$(basename "$PWD")
+mkdir dist
+TEMPDIR="$(mktemp -d)"
+read VERSION < NEWS
+echo "$PACKAGE-$VERSION: $TEMPDIR"
+hg archive "$TEMPDIR/$PACKAGE-$VERSION"
+DIR="$PWD"
+cd "$TEMPDIR"
+#rm "$PACKAGE-$VERSION/release.sh"
+tar czf "$DIR/dist/$PACKAGE-$VERSION.tar.gz" "$PACKAGE-$VERSION"
+tar cjf "$DIR/dist/$PACKAGE-$VERSION.tar.bz2" "$PACKAGE-$VERSION"
+cd "$PACKAGE-$VERSION"
+./setup.py bdist_rpm
+mv "dist/$PACKAGE-$VERSION-1."{noarch,src}.rpm "$DIR/dist"
+cd "$DIR"
+rm -fr "$TEMPDIR"
