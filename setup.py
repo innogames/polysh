118b7a54edfb0188317f303ff20f1289c16a6db6
g@localhost.localdomain <>
2007-07-27 22:10:03 +0200

    Install the man page even with ./setup.py install

--- setup.py
+++ setup.py
@@ -3,8 +3,17 @@
 import sys
 
 sys.path.insert(0, 'setuptools-0.6c6-py2.4.egg')
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, command
 from gsh.version import VERSION
+
+# Monkey patch the install command so that it also installs the man page
+# using the install_data command.
+vanilla_install_run = command.install.install.run
+def install_also_install_data(self):
+    vanilla_install_run(self)
+    self.run_command('install_data')
+
+command.install.install.run = install_also_install_data
 
 setup(name='gsh',
       version=VERSION,
