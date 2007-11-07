0937e401fa886370ab7b57262c23bba4e77d9829
Guillaume Chazarain <guichaz@yahoo.fr>
2007-11-07 15:10:18 +0100

    Propagate the EIO only on the first read. An EIO on a subsequent read may indicate we already read an error message like the SSH remote host identification change.

--- gsh/buffered_dispatcher.py
+++ gsh/buffered_dispatcher.py
@@ -63,6 +63,10 @@ class buffered_dispatcher(asyncore.file_dispatcher):
                     if e.errno == errno.EAGAIN:
                         # End of the available data
                         break
+                    elif e.errno == errno.EIO and new_data:
+                    	# Hopefully we could read an error message before the
+                    	# actual termination
+                        break
                     else:
                         raise
                 new_data += piece
