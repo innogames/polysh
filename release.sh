867995810d9d4f0d4a49258f190cbfefca4e96c3
Guillaume Chazarain <guichaz@gmail.com>
2008-03-10 22:12:41 +0100

    Updated release script

--- release.sh
+++ release.sh
@@ -6,13 +6,13 @@ set -e # Exit on error
 PACKAGE=$(basename "$PWD")
 mkdir dist
 TEMPDIR="$(mktemp -d)"
-VERSION=$(python -c 'from gsh.version import VERSION; print VERSION')
+VERSION=$(python -c "from $PACKAGE.version import VERSION; print VERSION")
 echo "$PACKAGE-$VERSION: $TEMPDIR"
-hg archive "$TEMPDIR/$PACKAGE-$VERSION"
+mkdir "$TEMPDIR/$PACKAGE-$VERSION"
+git archive HEAD | (cd "$TEMPDIR/$PACKAGE-$VERSION" && tar vx)
 DIR="$PWD"
 cd "$TEMPDIR"
 rm "$PACKAGE-$VERSION/release.sh"
-rm "$PACKAGE-$VERSION"/.hg*
 tar czf "$DIR/dist/$PACKAGE-$VERSION.tar.gz" "$PACKAGE-$VERSION"
 tar cjf "$DIR/dist/$PACKAGE-$VERSION.tar.bz2" "$PACKAGE-$VERSION"
 cd "$PACKAGE-$VERSION"
