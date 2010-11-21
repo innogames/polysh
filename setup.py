cfea529d671bae20ed8f772200ee2b2ecc0f25a7
Guillaume Chazarain <guichaz@gmail.com>
2010-11-21 19:03:19 +0100

    Migrated from setuptools to distutils.

--- setup.py
+++ setup.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 
-import sys
-
-sys.path.insert(0, 'setuptools-0.6c6-py2.4.egg')
-from setuptools import setup, find_packages, command
+from distutils.core import setup
 from gsh.version import VERSION
-
-# Monkey patch the install command so that it also installs the man page
-# using the install_data command.
-vanilla_install_run = command.install.install.run
-def install_also_install_data(self):
-    vanilla_install_run(self)
-    self.run_command('install_data')
-
-command.install.install.run = install_also_install_data
 
 setup(name='gsh',
       version=VERSION,
@@ -26,6 +14,6 @@ time and control them from a single command prompt.""",
       url='http://guichaz.free.fr/gsh',
       scripts=['bin/gsh'],
       data_files=[('share/man/man1', ['gsh.1'])],
-      packages=find_packages(),
+      packages=['gsh'],
       license='GPL'
 )
