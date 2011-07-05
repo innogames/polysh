2086815eccd7470e06a0510ff38926d21abef30a
Guillaume Chazarain <guichaz@gmail.com>
2011-07-05 23:57:26 +0200

    Fix RPM building on Fedora 15

---
+++ .install-rpm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# http://bugs.python.org/issue644744
+
+python setup.py install -O1 --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+# 'brp-compress' gzips the man pages without distutils knowing... fix this
+sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
