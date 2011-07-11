612801e95545c5f958bd191badcee1455c72fb75
Guillaume Chazarain <guichaz@gmail.com>
2011-07-11 22:36:46 +0200

    Ubuntu's RPM install does not compress man pages, so make the fix more generic

--- .install-rpm.sh
+++ .install-rpm.sh
@@ -3,5 +3,7 @@
 # http://bugs.python.org/issue644744
 
 python setup.py install -O1 --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
+
 # 'brp-compress' gzips the man pages without distutils knowing... fix this
-sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
+NEW_BASENAME="$(basename "$(find "$RPM_BUILD_ROOT" -type f -name "polysh.1*")")"
+set "s/polysh.1/$NEW_BASENAME/g" -i INSTALLED_FILES
