f8818be4390eb10d7514ea24a09c384443fc8d8e
Emre Hasegeli <emre@hasegeli.com>
2018-04-05 13:13:51 +0200

    Use setup.cfg file

--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
+"""Polysh - Setup script
+
+Copyright (c) 2018, InnoGames GmbH
+"""
 
 from setuptools import setup
 
-from polysh import VERSION
-
-setup(
-    name='polysh',
-    version='.'.join(map(str, VERSION)),
-    maintainer='InnoGames System Administration',
-    maintainer_email='it@innogames.com',
-    url='https://github.com/innogames/polysh',
-    data_files=[('share/man/man1', ['polysh.1'])],
-    packages=['polysh'],
-    long_description=(
-        'polysh is used to launch several remote shells on many machines at'
-        ' the same time and control them from a single command prompt.'),
-    entry_points={
-        'console_scripts': [
-            'polysh=polysh.main:main',
-        ],
-    },
-    license='GPL'
-)
+setup()
