e5fc872f39b428ac4b5cdaf8fa25899d493ceaef
Daniel Kröger <daniel.kroeger@innogames.com>
2023-06-07 08:58:43 +0200

    Bump version 0.14

--- polysh/__init__.py
+++ polysh/__init__.py
@@ -16,4 +16,4 @@ Copyright (c) 2020 InnoGames GmbH
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = (0, 13)
+VERSION = (0, 14)
